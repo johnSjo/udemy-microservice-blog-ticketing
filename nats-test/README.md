ba7c8ce1e2ad849df8bafdffe9ef17d182666b4e
John <john.sjostrom@valtech.com>
2022-06-08 16:35:02 +0200

    extracting Event classes to separate files

---
+++ nats-test/README.md
@@ -0,0 +1,3 @@
+
+remember to set up a port forward for the nats pod
+`kubectl port-forward [pod_name] 4222:4222`
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from 'crypto';
-import nats, { Message, Stan } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
+import { TicketCreatedListener } from './events/TicketCreatedListener';
 
 console.clear();
 
@@ -15,75 +16,8 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('order-service');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'order-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(
-        `Received event #${msg.getSequence()}, with data: ${JSON.parse(data)}`
-      );
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-
-  private _client: Stan;
-  protected _ackWait: number = 5000;
-
-  constructor(client: Stan) {
-    this._client = client;
-  }
-
-  subscriptionOptions() {
-    return this._client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this._ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen() {
-    const subscription = this._client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on('message', (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-
-    return typeof data === 'string'
-      ? JSON.parse(data)
-      : JSON.parse(data.toString('utf8'));
-  }
-}
